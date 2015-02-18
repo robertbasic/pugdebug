5677a4c6752209e8881c8b9b6991a710d01e6aa7
Robert Basic <robertbasic.com@gmail.com>
2015-02-18 12:25:20 +0100

    lets try have unit tests for this app

---
+++ pugdebug/tests/test_pugdebug.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+    pugdebug - a standalone PHP debugger
+    =========================
+    copyright: (c) 2015 Robert Basic
+    license: GNU GPL v3, see LICENSE for more details
+"""
+
+__author__="robertbasic"
+
+import sys
+import unittest
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeView
+from pugdebug.pugdebug import Pugdebug
+
+class PugdebugTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = QApplication(sys.argv)
+        self.pugdebug = Pugdebug()
+
+    def test_main_window_setup_correctly(self):
+        self.assertIsInstance(self.pugdebug.main_window, QMainWindow)
+        self.assertIsInstance(self.pugdebug.file_browser, QTreeView)
