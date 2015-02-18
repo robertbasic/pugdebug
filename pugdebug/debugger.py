1179a6aa34c4ab3f5fe10f436adb0a7b6d22e06d
Robert Basic <robertbasic.com@gmail.com>
2015-02-18 16:58:52 +0100

    initial connection to xdebug

--- pugdebug/debugger.py
+++ pugdebug/debugger.py
@@ -9,13 +9,46 @@
 
 __author__="robertbasic"
 
+import socket
+
 class PugdebugDebugger():
+
+    sock = None
+    address = None
+
+    is_running = False
 
     def __init__(self):
         pass
 
     def start_debug(self):
-        print('start')
+        if self.is_running:
+            print('already running')
+            return
+
+        self.is_running = True
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.settimeout(None)
+
+        try:
+            server.bind(('', 9000))
+        except server.error as socketerror:
+            print(socketerror)
+            print("Socket bind failed")
+
+        server.listen(5)
+
+        try:
+            print('Waiting for connection ...')
+            self.sock, self.address = server.accept()
+            self.sock.settimeout(None)
+            self.read_init_message()
+        except:
+            server.close()
+            raise
+
+        server.close()
 
     def stop_debug(self):
         print('stop')
@@ -28,3 +61,6 @@ class PugdebugDebugger():
 
     def step_out(self):
         print('out')
+
+    def read_init_message(self):
+        print(self.sock.recv(1024))
