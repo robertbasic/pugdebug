da29a2a598f77bedf6763b0e6a624627995e64c8
Robert Basic <robertbasic.com@gmail.com>
2015-02-25 15:10:18 +0100

    add some documentation strings

--- pugdebug/debugger.py
+++ pugdebug/debugger.py
@@ -32,6 +32,16 @@ class PugdebugDebugger(QObject):
     step_command_signal = pyqtSignal()
 
     def __init__(self):
+        """Init the debugger object
+
+        Create a PugdebugServer object used to communicate with xdebug client
+        through TCP.
+
+        Create a PugdebugMessageParser object used to parse the xml responses
+        from xdebug.
+
+        Connect signals to slots.
+        """
         super(PugdebugDebugger, self).__init__()
 
         self.server = PugdebugServer()
@@ -41,41 +51,50 @@ class PugdebugDebugger(QObject):
         self.server.last_message_read_signal.connect(self.handle_last_message_read)
 
     def start_debug(self):
+        """Start a debugging session
+
+        If the server is not connected, connect it.
+        """
         if not self.server.isListening():
             self.server.connect()
 
     def handle_init_message_read(self):
-        self.current_file = self.get_index_file()
+        """Handle when the init message from xdebug is read
 
+        At this point the server should have established a TCP connection and
+        the init message from xdebug client should be received.
+
+        Emit the custom debugging started signal.
+        """
         self.debugging_started_signal.emit()
 
     def handle_last_message_read(self):
+        """Handle when the latest message from xdebug is read
+
+        This should be called after a command is sent to xdebug
+        and a reply message is read.
+        """
         last_message = self.server.get_last_message()
         self.last_message = self.parser.parse_continuation_message(last_message)
 
         self.step_command_signal.emit()
 
     def stop_debug(self):
-        print('stop')
         self.server.close()
 
     def run_debug(self):
-        print('run')
         command = 'run -i %d' % self.get_transaction_id()
         self.server.command(command)
 
     def step_over(self):
-        print('over')
         command = 'step_over -i %d' % self.get_transaction_id()
         self.server.command(command)
 
     def step_into(self):
-        print('in')
         command = 'step_into -i %d' % self.get_transaction_id()
         self.server.command(command)
 
     def step_out(self):
-        print('out')
         command = 'step_out -i %d' % self.get_transaction_id()
         self.server.command(command)
 
--- pugdebug/pugdebug.py
+++ pugdebug/pugdebug.py
@@ -20,6 +20,14 @@ from pugdebug.models.file_browser import PugdebugFileBrowser
 class Pugdebug():
 
     def __init__(self):
+        """Initialize the application
+
+        Initializes all the different parts of the application.
+
+        Creates the PugdebugDebugger object, sets up the application UI,
+        connects signals to slots.
+        """
+
         self.debugger = PugdebugDebugger()
 
         self.main_window = PugdebugMainWindow()
@@ -33,12 +41,24 @@ class Pugdebug():
         self.connect_signals()
 
     def setup_file_browser(self):
+        """Setup the file browser
+
+        Sets the model on the file browser and hides the
+        not needed columns.
+        """
+
         model = PugdebugFileBrowser(self.main_window)
         self.file_browser.setModel(model)
         self.file_browser.setRootIndex(model.start_index)
         self.file_browser.hide_columns()
 
     def connect_signals(self):
+        """Connect all signals to their slots
+
+        Connect file browser signals, toolbar action signals,
+        debugger signals.
+        """
+
         self.connect_file_browser_signals()
 
         self.connect_toolbar_action_signals()
@@ -46,9 +66,23 @@ class Pugdebug():
         self.connect_debugger_signals()
 
     def connect_file_browser_signals(self):
+        """Connect file browser signals
+
+        Connects the file browser's activated signal to the
+        slot that gets called when a file browser item is activated.
+        """
         self.file_browser.activated.connect(self.file_browser_item_activated)
 
     def connect_toolbar_action_signals(self):
+        """Connect toolbar action signals
+
+        Connect signals that get emitted when the toolbar actions get triggered.
+
+        Connect signals when the start/stop debug actions are triggered are connected.
+
+        Connect signals when the run and step continuation commands are triggered are
+        connected.
+        """
         self.main_window.start_debug_action.triggered.connect(self.start_debug)
         self.main_window.stop_debug_action.triggered.connect(self.stop_debug)
         self.main_window.run_debug_action.triggered.connect(self.run_debug)
@@ -57,14 +91,34 @@ class Pugdebug():
         self.main_window.step_out_action.triggered.connect(self.step_out)
 
     def connect_debugger_signals(self):
+        """Connect debugger signals
+
+        Connect signal that gets emitted when the debugging is started, when the TCP connection
+        is established and the initial message from xdebug is read.
+
+        Connect signal that gets emitted when a step command is executed and the reply message
+        from xdebug is read.
+        """
+
         self.debugger.debugging_started_signal.connect(self.handle_debugging_started)
         self.debugger.step_command_signal.connect(self.handle_step_command)
 
     def file_browser_item_activated(self, index):
+        """Handle when file browser item gets activated
+
+        Find the path of the activated item and open that document.
+        """
         path = self.file_browser.model().filePath(index)
         self.open_document(path)
 
     def open_document(self, path):
+        """Open a document
+
+        If a document is not already open, open it and add it as a new
+        tab.
+
+        If the document is already open, focus the tab with that document.
+        """
         if not self.documents.is_document_open(path):
             document = self.documents.open_document(path)
 
@@ -75,6 +129,11 @@ class Pugdebug():
             self.document_viewer.focus_tab(path)
 
     def focus_current_line(self):
+        """Focus the current line
+
+        Focus the line where the debugger stopped in the file
+        that is being debugged.
+        """
         current_file = self.debugger.get_current_file()
         current_line = self.debugger.get_current_line()
 
@@ -83,19 +142,33 @@ class Pugdebug():
         doc = self.document_viewer.get_current_document()
         doc.move_to_line(current_line)
 
-    def start_debug(self):
-        self.debugger.start_debug()
-
     def handle_debugging_started(self):
+        """Handle when debugging starts
+
+        This handler should be called when the connection to
+        xdebug is established and the initial message from xdebug
+        is read.
+
+        Issue a step_into command to break at the first line.
+        """
         self.focus_current_line()
 
         self.step_into()
 
-    def handle_step_command(self):
-        self.focus_current_line()
+    def start_debug(self):
+        self.debugger.start_debug()
 
     def stop_debug(self):
         self.debugger.stop_debug()
+
+    def handle_step_command(self):
+        """Handle step command
+
+        This handler should be called when one of the step
+        commands is executed and the reply message from xdebug
+        is read.
+        """
+        self.focus_current_line()
 
     def run_debug(self):
         self.debugger.run_debug()
