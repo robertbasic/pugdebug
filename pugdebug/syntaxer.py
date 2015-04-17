a39e19593e292fb9a52e6b61861e6e8df114c00d
Robert Basic <robertbasic.com@gmail.com>
2015-04-17 10:37:16 +0200

    Separate formats block by block
    This way when QSyntaxHighlighter highlights a block, we can
    get the formats for that block only, no need to iterate over
    all formats.

--- pugdebug/syntaxer.py
+++ pugdebug/syntaxer.py
@@ -33,27 +33,30 @@ class PugdebugSyntaxer(QSyntaxHighlighter):
         self.formatter = formatter
         self.lexer = PhpLexer()
 
+        self.formatter.document = document
+
         highlight(self.document().toPlainText(), self.lexer, self.formatter)
 
     def highlightBlock(self, text):
         block = self.currentBlock()
+        block_number = block.blockNumber()
 
-        position = block.position()
+        block_format = None
 
-        if position != 0:
-            previous_block = block.previous()
-            self.buffer += previous_block.length()
+        if block_number in self.formatter.formats:
+            block_format = self.formatter.formats[block_number]
 
-        for format in self.formatter.formats:
-            start = format['start'] - self.buffer
-            end = format['end'] - self.buffer
-            self.setFormat(start, end, format['style'])
+        if block_format is not None:
+            for format in block_format:
+                self.setFormat(format['start'], format['end'], format['style'])
 
 class PugdebugFormatter(Formatter):
 
     styles = {}
 
-    formats = []
+    formats = {}
+
+    document = None
 
     def __init__(self):
         super(PugdebugFormatter, self).__init__()
@@ -68,15 +71,47 @@ class PugdebugFormatter(Formatter):
             self.styles[token] = format
 
     def format(self, tokensource, outfile):
-        self.formats = []
-        start = 0
+        """Format source file
+
+        Formats are separated block by block.
+        """
+        # Formats for every block, block by block
+        self.formats = {}
+        # Position of value in the source
+        current_value_position = 0
+        # The number of the current block
+        current_block_number = -1
+        # Where does text start in the current block
+        # Used to skip whitespace on the beginning of line
+        start_in_block = 0
+        # The text of the current block
+        block_text = ''
+
         for token, value in tokensource:
+            # Find the block which has the current value, by it's position
+            block = self.document.findBlock(current_value_position)
+            block_number = block.blockNumber()
+
+            # If we advanced a block
+            if current_block_number < block_number:
+                block_text = block.text()
+                # Where do we start in the current block, whitespace included?
+                start_in_block = len(block_text) - len(block_text.lstrip())
+                current_block_number = block_number
+
+            # If this block has no formats yet
+            if block_number not in self.formats:
+                self.formats[block_number] = []
+
             length = len(value)
-            end = start + length
+            end = start_in_block + length
+            # Format for the current value
             format = {
-                'start': start,
+                'start': start_in_block,
                 'end': end,
                 'style': self.styles[token]
             }
-            self.formats.append(format)
-            start += length
+            start_in_block += length
+            current_value_position += length
+
+            self.formats[block_number].append(format)
