40865be3d753fb9d9ef6f7a949ae2542af4d12a1
Robert Basic <robertbasic.com@gmail.com>
2015-05-02 20:04:24 +0200

    draw a rect in the statusbar with the status colour

--- pugdebug/gui/statusbar.py
+++ pugdebug/gui/statusbar.py
@@ -9,6 +9,7 @@
 
 __author__ = "robertbasic"
 
+from PyQt5.QtGui import QPainter, QColor, QPixmap
 from PyQt5.QtWidgets import QLabel, QWidget, QHBoxLayout
 
 
@@ -17,8 +18,10 @@ class PugdebugStatusBar(QWidget):
     def __init__(self):
         super(PugdebugStatusBar, self).__init__()
         self.label = QLabel(self)
+        self.light = QLabel(self)
 
         layout = QHBoxLayout()
+        layout.addWidget(self.light)
         layout.addWidget(self.label)
 
         self.setLayout(layout)
@@ -26,11 +29,22 @@ class PugdebugStatusBar(QWidget):
     def set_debugging_status(self, status):
         if status == 0:
             text = "Idle ..."
+            color = "gray"
         elif status == 1:
             text = 'Waiting for connection ...'
+            color = "orange"
         elif status == 2:
             text = 'Debugging stopped ...'
+            color = "red"
         elif status == 3:
             text = 'Debugging in progress ...'
+            color = "green"
 
         self.label.setText(text)
+
+        self.pixmap = QPixmap(10, 10)
+        color = QColor(color)
+        self.pixmap.fill(color)
+        painter = QPainter(self.pixmap)
+        painter.drawRect(0, 0, 10, 10)
+        self.light.setPixmap(self.pixmap)
