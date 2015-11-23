67c2aeac62efb20f0c36ead920febea6eee6b637
Mattia Barbon <mattia@barbon.org>
2015-11-23 17:38:25 +0100

    Report error after performing cleanups

--- pugdebug/debugger.py
+++ pugdebug/debugger.py
@@ -336,12 +336,12 @@ class PugdebugDebugger(QObject):
 
         Kill the current connection and stop debugging.
         """
-        error = error + " during %s action" % action
-        self.error_signal.emit(error)
-
         # The current connection is FUBAR so just set it to None
         self.current_connection = None
         self.stop_debug()
+
+        error = error + " during %s action" % action
+        self.error_signal.emit(error)
 
     def get_current_file(self):
         if 'filename' in self.step_result:
