add524e88d4d1a17ff88409b58233399d6aebe5e
Robert Basic <robertbasic.com@gmail.com>
2017-01-06 12:50:21 +0100

    Fuzzy file search attempt

--- pugdebug/gui/search.py
+++ pugdebug/gui/search.py
@@ -9,8 +9,12 @@
 
 __author__ = "robertbasic"
 
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import (QDialog, QLineEdit, QVBoxLayout, QFormLayout,
                              QListWidget)
+
+from pugdebug.models.file_search import PugdebugFileSearch
+from pugdebug.models.settings import get_setting
 
 class PugdebugFileSearchWindow(QDialog):
 
@@ -23,13 +27,21 @@ class PugdebugFileSearchWindow(QDialog):
 
         self.accepted.connect(self.open_file)
 
+        self.timer = QTimer()
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.search_files)
+
+        project_root = get_setting('path/project_root')
+        self.file_search = PugdebugFileSearch(self, project_root)
+
         self.setup_layout()
 
     def setup_layout(self):
         self.file_name = QLineEdit()
+        self.file_name.textEdited.connect(self.start_timer)
 
         self.files = QListWidget()
-        self.files.addItems(['1','2','3','4','5','6'])
+        self.files.itemActivated.connect(self.file_selected)
 
         search_layout = QFormLayout()
         search_layout.addRow("Search for:", self.file_name)
@@ -39,6 +51,17 @@ class PugdebugFileSearchWindow(QDialog):
         box_layout.addLayout(search_layout)
 
         self.setLayout(box_layout)
+
+    def start_timer(self, text):
+        self.timer.start(500)
+
+    def search_files(self):
+        self.files.clear()
+        files = self.file_search.search(self.file_name.text())
+        self.files.addItems(files)
+
+    def file_selected(self, item):
+        print(item.data(Qt.DisplayRole))
 
     def open_file(self):
         pass
